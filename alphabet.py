8d4dd54f39eba4a0fa813e2118669ec4026addd9
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-08-04 00:05:05 +0900

    알파벳 출력

---
+++ alphabet.py
@@ -0,0 +1,10 @@
+from string import ascii_lowercase, ascii_uppercase
+
+
+num = int(input().strip())
+print(num)
+
+if num == 0:
+    print(ascii_lowercase)
+elif num == 1:
+    print(ascii_uppercase)
